41946a15cf68db6b448e63c0d3d636dac7256d52
Alex Hryhoruk <oleksandr_grigor@ukr.net>
2024-09-25 19:06:35 -0600

    SearchModel

---
+++ PASV_WeatherForcast/Models/SearchModel.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  PASV_WeatherForcast
+//
+//  Created by Oleksandr Hryhoruk on 25/09/2024.
+//
+
+struct SearchModel {
+    let city: String
+    let time: String
+    let description: String
+    let temperature: Int
+    let highTemperature: Int
+    let lowTemperature: Int
+}
